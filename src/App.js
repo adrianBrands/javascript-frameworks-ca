cde444f910d1c2cf9daf38edbca76c04cede9851
adrianBrands <adrianbrandshaug@hotmail.com>
2023-03-02 22:39:08 +0100

    created routing for the pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-
+import { Routes, Route } from "react-router-dom";
+import { Home, Product, Cart, Checkout } from "./pages"
 
 
 function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/checkout" element={<Checkout />} />
+    </Routes>
+  )
+
+  
  
 }
 
