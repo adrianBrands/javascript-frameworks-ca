1273381d72368d6201ad12783eaef93d360ce87e
adrianBrands <adrianbrandshaug@hotmail.com>
2023-03-03 09:10:37 +0100

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import { Home, Product, Cart, Checkout } from "./pages"
+import { Home, Product, Cart, Checkout } from "./pages";
+import { Navbar, Footer } from "./components";
+
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/product/:id" element={<Product />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/checkout" element={<Checkout />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+      <Footer/>
+    </>
   )
 
   
