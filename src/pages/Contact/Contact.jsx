4c910b14d7e6394ac09ca99b2bfb5b0958f8b572
adrianBrands <adrianbrandshaug@hotmail.com>
2023-03-18 06:17:55 +0100

    installed formik and created contact form with validation

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,98 +1,83 @@
-import React, { useState } from "react";
-import { Container, Form, Button, FloatingLabel, Col } from "react-bootstrap";
-import { useForm } from 'react-hook-form';
+import { Container, Form, Button, FloatingLabel } from "react-bootstrap";
+import * as yup from 'yup';
+import { Formik } from 'formik';
+
+const schema = yup.object().shape({
+  fullName: yup.string().min(3).required(),
+  subject: yup.string().min(3).required(),
+  email: yup.string().email("invalid email").required(),
+  text: yup.string().min(3).required(),
+});
 
 export default function Contact(){
-  const { register, handleSubmit } = useForm();
-  //const [validated, setValidated] = useState(false);
 
-   /*const handleSubmit = (e) => {
-    const form = e.currentTarget;
-  
-    if(form.checkValidity() === false){
-      e.preventDefault();
-      e.stopPropagation();
-    }
-    setValidated(true)      
-    e.preventDefault();
-    
-  }*/
+return (
+  <Formik
+      validationSchema={schema}
+      onSubmit={console.log}
+      initialValues={{
+        fullName: '',
+        subject: '',
+        email: '',
+        text: '',
+      }}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        touched,
+        isValid,
+        errors,
+      }) => (
+      
+      <Container>
+        <h1 className="border-bottom">Contact</h1>
+        <Form className="mt-3" noValidate onSubmit={handleSubmit}>
+          <Form.Group  controlId="validationFormik01">
+            <FloatingLabel controlId="floatingInputName" label="Full name"  className="mb-3">
+              <Form.Control type="text" placeholder="fullName" name="fullName" value={values.fullName} onChange={handleChange} isValid={touched.fullName && !errors.fullName} isInvalid={!!errors.fullName}/>
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">Please provide more then 3 characters</Form.Control.Feedback>
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group className="mt-3">
+            <FloatingLabel controlId="floatingInputSubject" label="Subject" className="mb-3">
+              <Form.Control type="text" placeholder="subject" name="subject" value={values.subject} onChange={handleChange} isValid={touched.subject && !errors.subject} isInvalid={!!errors.subject}/>
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">Please provide more then 3 characters</Form.Control.Feedback>
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group>
+            <FloatingLabel controlId="floatingInputEmail" label="Email" className="mb-3">
+              <Form.Control type="email" placeholder="name@example.com" name="email" value={values.email} onChange={handleChange} isValid={touched.email && !errors.email} isInvalid={!!errors.email}/>
+              <Form.Control.Feedback type="invalid">Please provide a valid email address</Form.Control.Feedback>
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group>
+            <FloatingLabel controlId="floatingInputTextarea" label="Textarea" className="mb-3">
+            <Form.Control type="text" placeholder="text" name="text" value={values.text} onChange={handleChange} isValid={touched.text && !errors.text} isInvalid={!!errors.text}/>
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">Please provide more then 3 characters</Form.Control.Feedback>
+            </FloatingLabel>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
 
-function onSubmit(data) {
-    console.log(data);
-  }
-
-  return (
-    <Container>
-      <Form noValidate  onSubmit={handleSubmit(onSubmit)}>
-        <Form.Group controlId="validationCustom01">
-          <FloatingLabel controlId="floatingInputName" label="Full name"  className="mb-3">
-          <input {...register('firstName')} />
-     
-            <Form.Control required minLength="3" type="text" placeholder="full name"/>
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            <Form.Control.Feedback type="invalid">Please provide more then 3 characters</Form.Control.Feedback>
-          </FloatingLabel>
-        </Form.Group>
-        <Form.Group className="mt-3">
-          <FloatingLabel controlId="floatingInputSubject" label="Subject" className="mb-3">
-            <Form.Control required minLength="3" type="text" placeholder="subject"></Form.Control>
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            <Form.Control.Feedback type="invalid">Please provide more then 3 characters</Form.Control.Feedback>
-          </FloatingLabel>
-        </Form.Group>
-        <Form.Group>
-          <FloatingLabel controlId="floatingInputEmail" label="Email" className="mb-3">
-            <Form.Control type="email" placeholder="name@example.com"/>
-          </FloatingLabel>
-        </Form.Group>
-        <Form.Group>
-          <FloatingLabel controlId="floatingInputTextarea" label="Textarea" className="mb-3">
-            <Form.Control required minLength="3" as="textarea" placeholder="textarea"/>
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            <Form.Control.Feedback type="invalid">Please provide more then 3 characters</Form.Control.Feedback>
-          </FloatingLabel>
-        </Form.Group>
-
-        
-        
-        <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      </Form>
-    </Container>
-  )
-}
+  )}
+  </Formik>
+)}
 
 
 
 
-/*
-import { useForm } from 'react-hook-form';
 
-export default function Contact() {
-  const { register, handleSubmit } = useForm();
 
-  function onSubmit(data) {
-    console.log(data);
-  }
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('firstName')} />
-      <input {...register('lastName')} />
-      <select {...register('role')}>
-        <option value="developer">Developer</option>
-        <option value="designer">Designer</option>
-        <option value="admin">Admin</option>
-        <option value="other">Other</option>
-      </select>
-      <input type="submit" />
-    </form>
-  );
-}
-
-*/
 
 
 
