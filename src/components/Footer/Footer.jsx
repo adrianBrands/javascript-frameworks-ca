cc2777ac0fa6553ea51929471bdcf2543589fc7e
adrianBrands <adrianbrandshaug@hotmail.com>
2023-04-01 08:15:58 +0200

    cleaning

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
-import { FaStar  } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <div className="footer">
       <p className="d-flex text-warning justify-content-center align-items-center">
-        &copy;{new Date().getFullYear()} All rights reserved <FaStar  style={{color: 'f5eb3b'}} />
+        &copy;{new Date().getFullYear()} All rights reserved
       </p>
     </div>
   );
